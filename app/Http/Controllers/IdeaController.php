1d8c68309d789448dafb3544aedc707018d80d44
Nita Constantin <ctinnita88@gmail.com>
2026-05-09 22:01:19 +0300

    The Fundamentals - Form Request Classes

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\IdeaRequest;
 use App\Models\Idea;
 use Illuminate\Http\Request;
 
@@ -41,14 +42,9 @@ class IdeaController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)   
+    public function store(IdeaRequest $request)   
     {
         
-        request()->validate([
-            'description'=> ['required', 'min:10', '']
-        ]);
-
-
         Idea::create([
             'description' => request('description'),
             'state' => 'pending'
@@ -68,11 +64,9 @@ class IdeaController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Idea $idea)
+    public function update(IdeaRequest $request, Idea $idea)
     {
-        $idea->update([
-            'description' => request('description')
-        ]);
+        
         return redirect("/ideas/{$idea->id}");
     }
 
